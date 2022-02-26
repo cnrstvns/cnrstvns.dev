a8b970e38517896c6147a304e0ba51c5053ebb5d
Connor Stevens <conmail1224@gmail.com>
2022-02-25 21:06:00 -0800

    Image sizing

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function Index() {
     <>
       <Head title="Connor Stevens" />
       <div className="flex flex-col p-10 lg:flex-row min-h-screen bg-black items-start md:items-center justify-center space-y-10 lg:space-y-0 lg:space-x-10">
-        <img className="max-h-[14rem] md:max-h-[18rem] rounded-lg" src="/images/me.png" alt="me" />
+        <img className="max-h-[14rem] rounded-lg" src="/images/me.png" alt="me" />
         <div className="flex flex-col">
           <div className="text-4xl md:text-5xl lg:text-6xl font-semibold text-white">Connor Stevens</div>
           <div className="text-gray-400 mt-1">
