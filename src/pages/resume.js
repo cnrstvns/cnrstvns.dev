ffa45e18162dfe4af48de847ffdb8c247aa6741d
Connor Stevens <conmail1224@gmail.com>
2022-09-01 21:12:18 -0400

    Imports

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -1,5 +1,4 @@
-import * as React from 'react';
-
+import React from 'react';
 import Head from '../components/Head';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
