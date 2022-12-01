aca9411acd01e4e9cdbcb0f2c9ba231e8403e0e3
Connor Stevens <conmail1224@gmail.com>
2022-11-30 23:49:48 -0500

    Refactor footer

---
+++ src/app/hype/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function Hype() {
+  return (
+    <div className="flex flex-col pt-56 items-center min-h-screen px-6 lg:px-32 text-white m-auto">
+      <video
+        muted
+        autoPlay
+        controls
+        disablePictureInPicture
+        disableRemotePlayback
+        controlsList="nofullscreen nodownload"
+        src="/videos/index.mp4"
+        style={{ maxHeight: 500 }}
+      />
+    </div>
+  );
+}
