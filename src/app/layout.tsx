b836a349ae8936a9021d805fcc68d953f5d176e4
Connor Stevens <conmail1224@gmail.com>
2022-12-01 21:48:41 -0500

    FIx head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,27 +15,6 @@ interface Props {
 export default function RootLayout({ children }: Props) {
   return (
     <html className={inter.className} lang="en">
-      <Head>
-        <meta charSet="UTF-8" />
-        <meta content="width=device-width,initial-scale=1" name="viewport" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <meta content="My personal website." name="description" />
-        <meta content="connor stevens, connor, stevens" name="keywords" />
-        <meta content="#90D4FC" name="theme-color" />
-        <meta
-          content="Connor Stevens - My personal website."
-          property="og:title"
-        />
-        <meta content="Connor Stevens" property="og:site_name" />
-        <meta content="My personal website." property="og:description" />
-        <meta content="/images/me.webp" property="og:image" />
-        <meta name="msapplication-TileColor" content="#90D4FC" />
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
-
       {/* Navigation */}
       <Navbar />
 
