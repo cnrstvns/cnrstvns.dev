5b36b92ce0403d2c343068cf01a68e49ea2ebe80
Connor Stevens <conmail1224@gmail.com>
2022-12-05 19:41:33 -0500

    Adjust weight

--- src/components/resume/SectionTitle.tsx
+++ src/components/resume/SectionTitle.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function SectionTitle({ title }: Props) {
   return (
-    <div className="text-2xl font-bold mb-2 underline underline-offset-8 decoration-blue-300 pb-2">
+    <div className="text-2xl font-semibold mb-2 underline underline-offset-8 decoration-blue-300 pb-2">
       {title}
     </div>
   );
