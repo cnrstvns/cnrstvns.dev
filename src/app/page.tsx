4cb53e6c3f73597ffdf6887755347baa224240b0
Connor Stevens <conmail1224@gmail.com>
2023-02-02 15:42:54 -0500

    Add lanyard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,44 @@
 import Image from 'next/image';
 import React from 'react';
 import Link from '../components/shared/Link';
+import Listening from '../components/shared/Listening';
 
-export default function Index() {
+export default async function Index() {
   return (
-    <div className="flex flex-col p-10 lg:flex-row min-h-screen items-start md:items-center justify-center space-y-10 lg:space-y-0 lg:space-x-10">
-      <Image
-        className="rounded-lg"
-        src="/images/me.webp"
-        alt="me"
-        width={224}
-        height={224}
-      />
-      <div className="flex flex-col justify-start">
-        <div className="text-4xl md:text-5xl lg:text-6xl font-semibold text-white">
-          Connor Stevens
-        </div>
-        <div className="text-neutral-400 mt-1">Student, Software Engineer</div>
-        <div className="text-neutral-100 max-w-xl mt-4">
-          Third year Computing and Information Technologies student at RIT with
-          a passion for building software.
-        </div>
-        <div className="mt-4 space-x-2 lg:space-x-3 flex flex-wrap">
-          <Link size="sm" href="https://github.com/cnrstvns" external>
-            github
-          </Link>
-          <Link size="sm" href="https://linkedin.com/in/cnrstvns" external>
-            linkedin
-          </Link>
-          <Link size="sm" href="mailto:connor@hyper.co" external>
-            email
-          </Link>
+    <>
+      <div className="flex flex-col p-10 lg:flex-row min-h-screen items-start md:items-center justify-center space-y-10 lg:space-y-0 lg:space-x-10">
+        <Image
+          className="rounded-lg"
+          src="/images/me.webp"
+          alt="me"
+          width={224}
+          height={224}
+        />
+        <div className="flex flex-col justify-start">
+          <div className="text-4xl md:text-5xl lg:text-6xl font-semibold text-white">
+            Connor Stevens
+          </div>
+          <div className="text-neutral-400 mt-1">
+            Student, Software Engineer
+          </div>
+          <div className="text-neutral-100 max-w-xl mt-4">
+            Third year Computing and Information Technologies student at RIT
+            with a passion for building software.
+          </div>
+          <div className="mt-4 space-x-2 lg:space-x-3 flex flex-wrap">
+            <Link size="sm" href="https://github.com/cnrstvns" external>
+              github
+            </Link>
+            <Link size="sm" href="https://linkedin.com/in/cnrstvns" external>
+              linkedin
+            </Link>
+            <Link size="sm" href="mailto:connor@hyper.co" external>
+              email
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+      <Listening />
+    </>
   );
 }
---
+++ src/components/shared/Listening.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useLanyard } from 'use-lanyard';
+import React from 'react';
+import Link from 'next/link';
+import { DISCORD_ID } from '../../constants/lanyard';
+
+export default function Listening() {
+  const { state, data } = useLanyard(DISCORD_ID);
+
+  if (state !== 'loaded' || !data.spotify) return null;
+
+  return (
+    <div className="absolute bottom-0 text-white left-0 w-full bg-neutral-800 items-center flex px-6 py-4 md:py-3 space-x-2">
+      <div className="bg-green-400 rounded-full h-2 w-2  animate-pulse" />
+      <div className="">
+        <span className="font-light">Listening to: </span>
+        <Link
+          href={`https://open.spotify.com/track/${data.spotify.track_id}`}
+          className="font-medium hover:underline"
+        >
+          {data.spotify.song}{' '}
+        </Link>
+        <span className="font-light">by </span>
+        <span className="font-medium">{data.spotify.artist}</span>
+      </div>
+    </div>
+  );
+}
