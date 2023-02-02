ff869c7e81b341a5f862d82a75d34cb4bdd58a5d
Connor Stevens <conmail1224@gmail.com>
2023-02-02 15:56:06 -0500

    Update meta color

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -7,7 +7,7 @@ export default function Head() {
       <meta content="width=device-width,initial-scale=1;" name="viewport" />
       <meta content="My personal website." name="description" />
       <meta content="connor stevens, connor, stevens" name="keywords" />
-      <meta content="#90D4FC" name="theme-color" />
+      <meta content="#171717" name="theme-color" />
       <meta
         content="Connor Stevens - My personal website."
         property="og:title"
@@ -15,7 +15,7 @@ export default function Head() {
       <meta content="Connor Stevens" property="og:site_name" />
       <meta content="My personal website." property="og:description" />
       <meta content="/images/me.webp" property="og:image" />
-      <meta name="msapplication-TileColor" content="#90D4FC" />
+      <meta name="msapplication-TileColor" content="#171717" />
       <link rel="shortcut icon" href="/favicon.ico" />
       <title>Connor Stevens</title>
     </>
