d402a98298cfd26f04e96572dd017623f0667622
Connor Stevens <conmail1224@gmail.com>
2023-02-06 17:35:47 -0500

    Mobile updates

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import Image from 'next/image';
+import { faApple } from '@fortawesome/free-brands-svg-icons/faApple';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Row({ images }: { images: string[] }) {
   return (
-    <div className="flex w-full rounded overflow-hidden">
+    <div className="flex flex-col md:flex-row w-full md:rounded-sm overflow-hidden space-y-4 md:space-y-0">
       <Image
-        className="w-1/3"
+        className="md:w-1/3 rounded-sm md:rounded-none"
         src={`/images/photos/IMG_${images[0]}.jpeg`}
         width={1152}
         height={150}
         alt=""
       />
       <Image
-        className="w-1/3"
+        className="md:w-1/3 rounded-sm md:rounded-none"
         src={`/images/photos/IMG_${images[1]}.jpeg`}
         width={1152}
         height={150}
         alt=""
       />
       <Image
-        className="w-1/3"
+        className="md:w-1/3 rounded-sm md:rounded-none"
         src={`/images/photos/IMG_${images[2]}.jpeg`}
         width={1152}
         height={150}
@@ -33,21 +35,16 @@ export default function Photos() {
   return (
     <div className="flex flex-col pt-40 pb-20 min-h-screen px-6 lg:px-32 text-white">
       <div className="max-w-6xl mx-auto">
-        <div className="mb-10">
-          I have one favorite place on earth. And I take a lot of photos there.
-          Enjoy.
-        </div>
-
         <div className="flex-col space-y-4">
           <Image
-            className="rounded overflow-hidden"
+            className="rounded-sm overflow-hidden"
             src="/images/photos/IMG_2732.jpeg"
             width={1152}
             height={150}
             alt=""
           />
           <Image
-            className="rounded overflow-hidden"
+            className="rounded-sm overflow-hidden"
             src="/images/photos/IMG_2425.jpeg"
             width={1152}
             height={150}
@@ -57,6 +54,10 @@ export default function Photos() {
           <Row images={['2552', '2197', '2131']} />
           <Row images={['3086', '2731', '2412']} />
           <Row images={['3080', '2148', '2806']} />
+        </div>
+
+        <div className="mt-16 text-center w-full text-neutral-300">
+          Shot on iPhone <FontAwesomeIcon icon={faApple} />
         </div>
       </div>
     </div>
