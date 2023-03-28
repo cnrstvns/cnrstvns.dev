86f71e50bc39618af7592792d1ee1fa11706ff11
Connor Stevens <conmail1224@gmail.com>
2023-03-28 14:16:47 -0400

    Next 13.2.4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import React, { ReactNode } from 'react';
+import { type Metadata } from 'next';
 import Footer from '../components/shared/Footer';
 import Navbar from '../components/shared/Navbar';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -7,6 +8,20 @@ import '../styles/globals.css';
 interface Props {
   children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: 'Connor Stevens',
+  applicationName: 'Connor Stevens',
+  description: 'My personal website.',
+  keywords: ['connor stevens', 'connor', 'stevens'],
+  themeColor: '#171717',
+  openGraph: {
+    title: 'Connor Stevens - My personal website.',
+    siteName: 'Connor Stevens',
+    description: 'My personal website.',
+    images: ['/images/me.webp'],
+  },
+};
 
 export default function RootLayout({ children }: Props) {
   return (
