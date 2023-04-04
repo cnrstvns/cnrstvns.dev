970b4c0d4e91b3d70e39ee92a4f09beea44f7348
Connor Stevens <conmail1224@gmail.com>
2023-04-04 10:40:00 -0400

    Massively simplify eslint

--- src/app/(footer)/photos/page.tsx
+++ src/app/(footer)/photos/page.tsx
@@ -57,7 +57,8 @@ export default function Photos() {
         </div>
 
         <div className="mt-16 text-center w-full text-neutral-300">
-          Shot on iPhone <FontAwesomeIcon icon={faApple} />
+          Shot on iPhone
+          <FontAwesomeIcon icon={faApple} />
         </div>
       </div>
     </div>
