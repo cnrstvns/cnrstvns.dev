e4d92b94288049e5e1cb6f54d9798e6a7b6aa7ee
Connor Stevens <conmail1224@gmail.com>
2023-05-01 19:36:00 -0400

    Use params instead

--- src/app/(footer)/posts/[slug]/page-view.ts
+++ src/app/(footer)/posts/[slug]/page-view.ts
@@ -1,17 +1,17 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { useParams } from 'next/navigation';
 import { useEffect } from 'react';
 
 export function PageView() {
-  const pathname = usePathname();
+  const params = useParams();
 
   useEffect(() => {
     if (typeof window === 'undefined') return;
     if (process.env.NEXT_PUBLIC_VERCEL_ENV !== 'production') return;
 
-    fetch(`/api/page-view?page=${pathname}`);
-  }, [pathname]);
+    fetch(`/api/page-view?page=${params?.slug}`);
+  }, [params]);
 
   return null;
 }
