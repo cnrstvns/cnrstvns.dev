ff487c0b96055655b73abec7b343a1f5f7b0564d
Connor Stevens <conmail1224@gmail.com>
2023-05-01 20:10:10 -0400

    Fonts

--- src/pages/api/og-image.tsx
+++ src/pages/api/og-image.tsx
@@ -13,10 +13,23 @@ function isString(str: unknown): str is string {
   return typeof str === 'string';
 }
 
+const openSansRegular = fetch(
+  new URL('../../styles/fonts/OpenSans-Regular.ttf', import.meta.url),
+).then((res) => res.arrayBuffer());
+
+const openSansMedium = fetch(
+  new URL('../../styles/fonts/OpenSans-SemiBold.ttf', import.meta.url),
+).then((res) => res.arrayBuffer());
+
 export default async function generateOGImage(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
+  const [openSansRegularData, openSansMediumData] = await Promise.all([
+    openSansRegular,
+    openSansMedium,
+  ]);
+
   if (!req.url) return res.status(404).end();
 
   const slug = new URL(req.url).searchParams.get('slug');
@@ -35,9 +48,17 @@ export default async function generateOGImage(
 
   return new ImageResponse(
     (
-      <div tw="flex w-[1200px] h-[600px]">
+      <div
+        tw="flex w-[1200px] h-[600px] overflow-hidden"
+        style={{ fontFamily: 'Open Sans Regular' }}
+      >
         <div tw="flex flex-col text-white w-full h-full px-24 justify-center bg-neutral-900">
-          <div tw="flex text-8xl font-bold mb-4">{post.title}</div>
+          <div
+            tw="flex text-8xl font-bold mb-4"
+            style={{ fontFamily: 'Open Sans Medium' }}
+          >
+            {post.title}
+          </div>
           <div tw="flex text-4xl text-neutral-300 mb-4">{post.description}</div>
           <div tw="flex text-4xl text-neutral-400 mb-4">{viewCount} views</div>
           <div tw="flex text-3xl text-neutral-200 mb-4">
@@ -51,6 +72,16 @@ export default async function generateOGImage(
     {
       width: 1200,
       height: 600,
+      fonts: [
+        {
+          name: 'Open Sans Regular',
+          data: openSansRegularData,
+        },
+        {
+          name: 'Open Sans Medium',
+          data: openSansMediumData,
+        },
+      ],
     },
   );
 }
