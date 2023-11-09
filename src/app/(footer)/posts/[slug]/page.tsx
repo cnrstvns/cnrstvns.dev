86f02d59042b365760e1e1282f5a66ed89801266
Connor Stevens <conmail1224@gmail.com>
2023-11-08 22:34:03 -0500

    Remove pages directory

--- src/app/(footer)/posts/[slug]/page.tsx
+++ src/app/(footer)/posts/[slug]/page.tsx
@@ -38,14 +38,14 @@ export function generateMetadata({
       description,
       type: 'article',
       url: `https://cnrstvns.dev/posts/${slug}`,
-      images: [`https://cnrstvns.dev/api/og-image?slug=${slug}`],
+      images: [`https://cnrstvns.dev/og-image?slug=${slug}`],
     },
     twitter: {
       card: 'summary_large_image',
       title,
       description,
       creator: '@cnrstvns',
-      images: [`https://cnrstvns.dev/api/og-image?slug=${slug}`],
+      images: [`https://cnrstvns.dev/og-image?slug=${slug}`],
     },
     metadataBase:
       process.env.NODE_ENV === 'production'
