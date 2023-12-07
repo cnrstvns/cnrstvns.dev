465e5b20971fa1ac1bd709f6050edb3911c4c34b
Connor Stevens <conmail1224@gmail.com>
2023-12-07 18:15:52 -0500

    Add perf insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React, { type PropsWithChildren } from 'react';
 import { type Metadata, type Viewport } from 'next';
 import { Navbar } from '@/components/shared/Navbar';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import '../styles/globals.css';
 
@@ -37,6 +38,7 @@ export default function RootLayout({ children }: PropsWithChildren) {
 
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
